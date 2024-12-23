49dc603a1d5a2061690ff90a82f526ec0c7d8aa9
felipetudeladm <felipetudeladm@gmail.com>
2024-12-23 19:59:02 -0300

    Configuração da seed e estilização da roleta

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // CONFIGURAÇÕES começo
-const seed = 0.1
+const seed = 0.5
 const pessoas = [
     "alexandre",
     "leo",
@@ -9,6 +9,16 @@ const pessoas = [
     "andrea",
     "duda",
 ]
+
+const pessoasAtributos = {
+    "alexandre": {nome: "Alexandre", presentes: null},
+    "leo": {nome: "Léo Amaro", presentes: null},
+    "felipe": {nome: "Felipe", presentes: null},
+    "leonardo": {nome: "Leonardo", presentes: null},
+    "livia": {nome: "Lívia", presentes: null},
+    "andrea": {nome: "Andréa", presentes: ["Camisa"]},
+    "duda": {nome: "Duda", presentes: null},
+}
 
 // CONFIGURAÇÕES fim
 
@@ -36,46 +46,65 @@ function embaralhar(arr) {
     return arr;
 }
 
+console.log(pessoas)
 const pessoasEmbaralhadas = embaralhar(pessoas)
-
-console.log(pessoasEmbaralhadas)
 
 
 function obterAmigoSecreto(nome) {
-
-    const sorteio = {
-        felipe: "alexandre",
-        livia: "leo",
-        leonardo: "felipe",
-
+    const indexNome = pessoasEmbaralhadas.indexOf(nome)
+    if (indexNome == pessoasEmbaralhadas.length - 1) {
+        return pessoasEmbaralhadas[0]
     }
-
-    return sorteio[nome]
-
+    return pessoasEmbaralhadas[indexNome + 1]
 }
 
 
 function onClickEnviar() {
 
+    console.log({pessoasEmbaralhadas})
+
     const divTelaInicial = document.getElementById("telainicial")
     const divTelaRoleta = document.getElementById("telaroleta")
     const divPessoaSorteada = document.getElementById("pessoasorteada")
-
-    // troca de tela começo
+    
+    // troca de tela começo 
     divTelaInicial.style.display = 'none'
     divTelaRoleta.style.display = 'block'
     // troca de tela fim
 
     // pegar nome do fomulário começo
-    const formInput = document.getElementById("idnome").value
-    const nome = formInput.value // todo: como garantir que a pessoa digitará o nome corretamente?
+    const formInput = document.getElementById("idnome")
+    const nomeLogin = formInput.value // todo: como garantir que a pessoa digitará o nome corretamente?
+    const sorteado = obterAmigoSecreto(nomeLogin)
     // pegar nome do fomulário fim
 
-    // const sorteado = obterAmigoSecreto(nome)
+    // OBTER OS NOMES RESTANTES
 
-    // divPessoaSorteada.innerText = sorteado
+    let pessoasQueParticiparaoDoSorteio = pessoas.filter(nome => nome != nomeLogin)
 
+    if (pessoasQueParticiparaoDoSorteio[5] != sorteado) {
+        const indexSorteado = pessoasQueParticiparaoDoSorteio.indexOf(sorteado)
+        pessoasQueParticiparaoDoSorteio[indexSorteado] = pessoasQueParticiparaoDoSorteio[5]
+        pessoasQueParticiparaoDoSorteio[5] = sorteado
+    }
 
+    // COLOCAR OS NOMES NA DIVS
+    for (let i = 0; i < 6; i++) {
+        const divPremio = document.querySelector('.premio' + (i+1))
+        const pessoa = pessoasQueParticiparaoDoSorteio[i]
+        console.log(pessoa)
+        divPremio.innerText = pessoasAtributos[pessoa].nome
+    }
+
+    // COLOCAR O SORTEADO NA FATIA QUE É SORTEADA
+
+    divPessoaSorteada.innerText = pessoasAtributos[sorteado].nome
+    function exibirResultado(){
+        const divResultadoSorteio = document.getElementById("resultadosorteio")
+        divResultadoSorteio.style.display = "block"
+    }
+
+    setTimeout(exibirResultado, 3000)
 }
 
 function playOnClick() {
